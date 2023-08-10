48059653ed8a76dcc341466f4192c5873a9f1eae
romulo.valle@saudedigital.tech <romulo.valle@saudedigital.tech>
2023-08-10 20:36:13 +0000

    quantidade_post update

--- models/example/test_dbt_model.sql
+++ models/example/test_dbt_model.sql
@@ -3,12 +3,12 @@
 
 with source_especialidade as (
 
-    select * from (SELECT nome_do_usuario, tipo_de_conteudo, COUNT(*) as Quantidade
+    select * from (SELECT nome_do_usuario, COUNT(*) as quantidade_post
     FROM {{source('prescricao', 'df_sanofi_doencas_keywords')}}
     where tipo_de_conteudo = 'POST'
-    group by nome_do_usuario, tipo_de_conteudo
-    order by quantidade)as subquery_alias
-    where quantidade > 20
+    group by nome_do_usuario
+    order by quantidade_post)as subquery_alias
+    where quantidade_post > 20
 )
 
 select *
